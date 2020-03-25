8178c541c00f5370cc2fc7d4353f059b0d0d2351
Acer <gsathiss@gmail.com>
2020-03-25 21:17:40 +0530

    Modified h2 config from file to in memory

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO SAT_DICT (id, word, meaning) VALUES
+  (1, 'slump', 'reduce|decrease'),
+  (2, 'imposter', 'fraud'),
+  (3, 'snap', 'quick|tamil-murithal');
+  
+  
+INSERT INTO SAT_HEROES (id, name) VALUES
+  (11, 'Dr Nice'),
+  (12, 'Narco'),
+  (13, 'Bombasto'),
+  (14, 'Celeritas'),
+  (15, 'Magneta'),
+  (16, 'RubberMan'),
+  (17, 'Dynama'),
+  (18, 'Dr IQ'),
+  (19, 'Magma'),
+  (20, 'Tornado');
